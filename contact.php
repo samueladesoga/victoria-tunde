2062cf1a4f9198e8271c8a2e3a83d6cecf4d057a
Samuel Adesoga <samuel@thoughstudiong.com>
2018-12-24 15:46:27 +0000

    added initial files

---
+++ contact.php
@@ -0,0 +1,56 @@
+<?php
+if($_POST)
+{
+    $to_email       = "info@victoriatunde.com"; //Recipient email, Replace with own email here
+   
+    //check if its an ajax request, exit if not
+    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+       
+        $output = json_encode(array( //create JSON data
+            'type'=>'error',
+            'text' => 'Sorry Request must be Ajax POST'
+        ));
+        die($output); //exit script outputting json data
+    }
+   
+    //Sanitize input data using PHP filter_var().
+    $name      = filter_var($_POST["user_name"], FILTER_SANITIZE_STRING);
+    $email     = filter_var($_POST["user_email"], FILTER_SANITIZE_EMAIL);
+    $message   = filter_var($_POST["msg"], FILTER_SANITIZE_STRING);
+    $subject   = "Contact message from Victoria Tunde's Website";
+   
+    //additional php validation
+    if(strlen($name)<4){ // If length is less than 4 it will output JSON error.
+        $output = json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
+        die($output);
+    }
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){ //email validation
+        $output = json_encode(array('type'=>'error', 'text' => 'Please enter a valid email!'));
+        die($output);
+    }
+    if(strlen($message)<3){ //check emtpy message
+        $output = json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
+        die($output);
+    }
+   
+    //email body
+    $message_body = $message."\r\n\r\n-".$name."\r\nEmail : ".$email ;
+   
+    //proceed with PHP email.
+    $headers = 'From: '.$name.'' . "\r\n" .
+    'Reply-To: '.$email.'' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+   
+    $send_mail = mail($to_email, $subject, $message_body, $headers);
+   
+    if(!$send_mail)
+    {
+        //If mail couldn't be sent output error. Check your PHP email configuration (if it ever happens)
+        $output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
+        die($output);
+    }else{
+        $output = json_encode(array('type'=>'message', 'text' => 'Hi '.$name .' Thank you for your email'));
+        die($output);
+    }
+}
+?>
